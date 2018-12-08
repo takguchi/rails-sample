c93602e951327b7586c2641728b38e6b5ec67ede
takguchi <takahiro89baka@gmail.com>
2018-12-08 10:15:07 +0900

    update board_controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,4 +5,15 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    Board.create(board_params)
+    binding.pry
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
 end
